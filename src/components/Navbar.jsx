5bb0956c540071289dd6cf97fd3bab67cc456ea8
Nisarg Mistry <mistry.nisarg.24@gmail.com>
2026-03-20 11:05:39 +0530

    Image FIx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <nav>
             <header className="body-font bg-white">
                 <div className="container mx-auto flex flex-wrap p-4 md:flex-row items-center align-middle text-center">
-                        <img src="../../../public/Favicon.png" width={25}/>
+                        <img src="/Favicon.png" width={25}/>
                     <span className="flex title-font font-medium items-center text-gray-900 mb-0 ">
                         <span className="ml-3 text-2xl">Password Manager</span>
                     </span>
